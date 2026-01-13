8a94ce0dd341b38295e64f7321d68dd64ad76108
FirasJebeniani <firas.jebeniani@ensi-uma.tn>
2026-01-13 18:50:31 +0100

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,15 +5,16 @@ import { FormContainer } from './form-container/form-container';
 
 @Component({
   selector: 'app-root',
+  standalone: true, 
   imports: [
     RouterOutlet,
     NavBar,
     FormContainer
-    
   ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'] 
 })
 export class App {
   protected readonly title = signal('Projet ossec');
 }
+
